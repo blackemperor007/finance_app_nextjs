76192897d0aee12061c4acfb8deabac9ff7cc1e5
Pagnol_wesley <emperorb007@gmail.com>
2025-02-24 13:24:47 +0100

    notification

--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import Wrapper from '../components/Wrapper';
 import { useUser } from '@clerk/nextjs';
 import EmojiPicker from 'emoji-picker-react';
+import { addBudget } from '../actions'
+import Notification from '../components/Notification';
 
 
 const page = () => {
@@ -11,8 +13,48 @@ const page = () => {
     const [budgetAmount, setBudgetAmount] = useState<string>("")
     const [selectedEmoji, setSelectedEmoji] = useState<string>("")
     const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false)
+
+    const [notification, setNotification] = useState<string>("")
+    const closeNotification = () => (
+        setNotification("")
+    )
+
+    const handleEmojiSelect = (emojiObject: { emoji: string }) => {
+        setSelectedEmoji(emojiObject.emoji)
+        setShowEmojiPicker(false)
+    }
+
+    const handleAddBudget = async () => {
+        
+        try {
+            const amount = parseFloat(budgetAmount)
+            if (isNaN(amount) || amount <= 0) {
+                throw new Error("Veuillez entrer une somme valide")
+            }
+            await addBudget(
+                user?.primaryEmailAddress?.emailAddress as string,
+                budgetName, 
+                amount, 
+                selectedEmoji,
+            )
+            const modal = document.getElementById("my_modal_3") as HTMLDialogElement
+
+            if (modal) {
+                modal.close()
+            }
+            
+            
+        } catch (error) {
+            console.error("Erreur lors de l'ajout du budget:", error)
+        }
+    }
+
     return (
         <Wrapper>
+            {notification && (
+                <Notification message={notification} onclose={closeNotification}></Notification>
+            )}
+
             <button className="btn" onClick={() => (document.getElementById('my_modal_3') as HTMLDialogElement).showModal()}>
                 Nouveau Budget
             </button>
@@ -46,12 +88,18 @@ const page = () => {
                             className='input input-bordered mb-3'
                             required
                         />
-                        <button className='btn' onClick={() => setShowEmojiPicker(!showEmojiPicker)}> {selectedEmoji || "selecttionnez un emoji 👍"} </button>
+                        <button className='btn mb-3' onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
+                            {selectedEmoji || "selectionnez un emoji 👍"}
+                        </button>
                         {showEmojiPicker && (
-                            <EmojiPicker />
+                            <div className="flex justify-center items-center my-4">
+                                <EmojiPicker onEmojiClick={handleEmojiSelect} />
+                            </div>
                         )}
 
-                        <button className='btn'>
+                        <button 
+                        onClick={handleAddBudget}
+                        className='btn'>
                             Ajouter budget
                         </button>
                     </div>
